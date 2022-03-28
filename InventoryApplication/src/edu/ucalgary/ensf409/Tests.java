c3ad6d47f306cce2030660e43aa2bd77e9fb5a30
Muhammad-I-Hasan <gsadu683@gmail.com>
2022-03-27 19:56:12 -0600

    added comments just for testing

--- InventoryApplication/src/edu/ucalgary/ensf409/Tests.java
+++ InventoryApplication/src/edu/ucalgary/ensf409/Tests.java
@@ -5,5 +5,14 @@ import static org.junit.Assert.*;
 
 public class Tests {
 	
-
+	/*
+	 * test:
+	 * all getters
+	 * all setters
+	 * all constructors
+	 * makeHamper - does not return null //maybe
+	 * we wouldn't check any DB updates - updateDB in ClientList and updateDB in FoodInv
+	 * test exceptions if we have any - finalizeOrder (insufficientInventoryException)
+	 * fdsfsdfsdfsdfsdfsdfsfsddfsdfsdsd
+	 */
 }

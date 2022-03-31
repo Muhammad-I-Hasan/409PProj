60a667a22b53bf050c1209b1b91dfcae6b9f813e
sas318sha <kobrusevsasha318@gmail.com>
2022-03-31 12:20:36 -0600

    Merge branch 'main' of https://github.com/MylesPribeg/409Proj.git into Sasha

---
+++ InventoryApplication/src/edu/ucalgary/ensf409/Nutrition.java
@@ -0,0 +1,45 @@
+package edu.ucalgary.ensf409;
+
+public class Nutrition {
+	private final int GRAIN;
+	private final int FRUITSVEGGIES;
+	private final int PROTEIN;
+	private final int OTHER;
+	private final int CALORIES;
+	
+	
+	Nutrition(int grain, int fruitsveggies, int protein, int calories,int other){
+		this.GRAIN = grain;
+		this.FRUITSVEGGIES = fruitsveggies;
+		this.PROTEIN = protein;
+
+		this.OTHER = other;
+		this.CALORIES = calories;
+		
+	}
+	public int getGrain() {
+		return GRAIN;
+	}
+
+	public int getFruitsVeggies() {
+		return FRUITSVEGGIES;
+	}
+
+	public int getProtein() {
+		return PROTEIN;
+	}
+
+	public int getOther() {
+		return OTHER;
+	}
+
+	public int getCalories() {
+		return CALORIES;
+	}
+
+	
+	
+	
+	
+	
+}
--- InventoryApplication/src/edu/ucalgary/ensf409/NutritionTest.java
+++ InventoryApplication/src/edu/ucalgary/ensf409/NutritionTest.java
@@ -1,5 +1,61 @@
 package edu.ucalgary.ensf409;
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+
 
 public class NutritionTest {
+	
+	
+	public int[] nutritionData = {
+            100,//grains
+            200,//fruits and veggies		
+            300,//protein
+            400,//calories
+            500//other
+    };
+	
+	@Test
+	public void constructorGoodData() {
+	
+		Nutrition test = new Nutrition(nutritionData[0],nutritionData[1],nutritionData[2],nutritionData[3],nutritionData[4]);
+		assertNotNull("AccessLog constructor did not create an object when given a valid inputs",test);
 
-}
+	}
+	@Test
+	public void testGetGrain() {
+		int expectedGrain = nutritionData[0];
+		Nutrition test = new Nutrition(nutritionData[0],nutritionData[1],nutritionData[2],nutritionData[3],nutritionData[4]);
+		int grain = test.getGrain();
+		assertEquals("getGrain did not return the correct amount of grain",expectedGrain,grain);
+	}
+	@Test
+	public void testGetFruitesAndVeggies() {
+		int expectedFruitesAndVeggies = nutritionData[1];
+		Nutrition test = new Nutrition(nutritionData[0],nutritionData[1],nutritionData[2],nutritionData[3],nutritionData[4]);
+		int FruitesAndVeggies = test.getFruitsVeggies();
+		assertEquals("getFruitsVeggies did not return the correct result",expectedFruitesAndVeggies,FruitesAndVeggies);
+	}
+	 
+	@Test
+	public void testGetProteins() {
+		int expected = nutritionData[2];
+		Nutrition test = new Nutrition(nutritionData[0],nutritionData[1],nutritionData[2],nutritionData[3],nutritionData[4]);
+		int result = test.getProtein();
+		assertEquals("getProtein did not return the correct result",expected,result);
+	}
+	@Test
+	public void testGetCalories() {
+		int expected = nutritionData[3];
+		Nutrition test = new Nutrition(nutritionData[0],nutritionData[1],nutritionData[2],nutritionData[3],nutritionData[4]);
+		int result = test.getCalories();
+		assertEquals("getCalories did not return the correct result",expected,result);
+	}
+	@Test
+	public void testGetOthers() {
+		int expected = nutritionData[4];
+		Nutrition test = new Nutrition(nutritionData[0],nutritionData[1],nutritionData[2],nutritionData[3],nutritionData[4]);
+		int result = test.getProtein();
+		assertEquals("getProtein did not return the correct result",expected,result);
+	}
+}	

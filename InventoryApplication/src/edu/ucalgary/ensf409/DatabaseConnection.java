38f63b039f9410cc0fb87032673d244865daf29e
sas318sha <kobrusevsasha318@gmail.com>
2022-04-10 16:25:19 -0600

    made database class

---
+++ InventoryApplication/src/edu/ucalgary/ensf409/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package edu.ucalgary.ensf409;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class DatabaseConnection {
+	private final String USERNAME;
+	private final String PASSWORD;
+	private final String CONNECTION;
+	protected Connection dbConnect;
+    protected ResultSet results;
+	
+	
+	DatabaseConnection(String name, String password, String connection){
+		this.USERNAME = name;
+		this.PASSWORD = password;
+		this.CONNECTION = connection;
+	}
+	
+	public void closeDB() {
+		try {
+			dbConnect.close();
+			results.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void initializeConnection(){
+    	try{
+            dbConnect = DriverManager.getConnection(this.CONNECTION,this.USERNAME,this.PASSWORD);
+           // System.out.println("got connection");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+/***********ADD CODE HERE***********/                
+
+    }
+	public abstract void loadFromDB();
+	public abstract void updateDB();
+	
+	
+}

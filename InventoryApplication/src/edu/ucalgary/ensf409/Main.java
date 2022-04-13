31e789def3d690fb9a04ccc26d8e43ba3f9b9f5d
Muhammad-I-Hasan <gsadu683@gmail.com>
2022-04-12 19:37:48 -0600

    added small tests for exception

--- InventoryApplication/src/edu/ucalgary/ensf409/Main.java
+++ InventoryApplication/src/edu/ucalgary/ensf409/Main.java
@@ -2,78 +2,49 @@ package edu.ucalgary.ensf409;
 
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws InsufficientInventoryException{
+		
+		//System.out.println("bruh");
+		
 		ClientList cl = new ClientList();
 		FoodInv inv = new FoodInv();
 		cl.loadFromDB();
 		inv.loadFromDB();
 
-		// checking inv
-//		for (Food i :inv.getFoodList()) {
-//			System.out.println(i.getName());
-//		}
-
 		Order order1 = new Order(inv);
-		Order order2 = new Order(inv);
-
-		Household one = new Household();
+		Household one = new Household("one");
 		one.addClient(cl.getClient(1));
-		one.addClient(cl.getClient(3));
-		one.addClient(cl.getClient(4));
-
 		
-//		one.addClient(cl.getClient(2));
-		// checking client list
-		/*
-		 * for (Client i : one.getClientList()) System.out.println(i.getClientType());
-		 * 
-		 */
-
-//		Household two = new Household();
-//		two.addClient(cl.getClient(1));
-//		two.addClient(cl.getClient(1));
-//		two.addClient(cl.getClient(1));
-
+		Household two = new Household("two");
+		two.addClient(cl.getClient(1));
+	
 		order1.addHousehold(one);
-
-//		order2.addHousehold(one);
-//		order2.addHousehold(one);
-
-		// checking two argument constructor
-//		Household[] list = { one, two };
-//		Order order3 = new Order(inv, list);
-
+		order1.addHousehold(two);
 		
-		// checking inv and checking remove
-//		
-//		for (Food i: order1.getInventory().getFoodList()) {
-//			System.out.println(i.getName()); 
-//		}
-//		order3.getInventory().remove(0);
-//		order3.getInventory().remove(1);
-//		System.out.println("BREAK---------------------------------------------------------------"); 
-//		for (Food i: order3.getInventory().getFoodList()) {
-//			System.out.println(i.getName()); 
-//		}
+		for (Food i: inv.getFoodList()) {
+			System.out.println(i.getName()); 
+		}
+		System.out.println("dsdasdasasd"); 
+		
 		
 		try {
 			order1.makeAndFinalizeOrder();
 		} catch (InsufficientInventoryException e) {
-			// TODO Auto-generated catch block
+			inv = order1.getInventory();
+			/*for (Household i: order1.getHouseholds()) {
+				i.getHamper().getContent().clear();
+			}*/
 			e.printStackTrace();
+			//throw new InsufficientInventoryException();
 		}
 		
 		for (Food i: inv.getFoodList()) {
 			System.out.println(i.getName()); 
 		}
 		 
-		 /*
-		 * for (Household i: order3.houseHolds) {//only if houseHolds is public (for
-		 * testing) System.out.println(i.getTotalNeeds().getCalories()); }
-		 */
-		
-		
+		OrderForm form = new OrderForm(order1);
+		System.out.println(form.printOrder()); 
+
 	}
 
 }

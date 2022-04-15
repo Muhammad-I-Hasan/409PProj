7005a2cb5d9f436f6e7dcf6851ed75ab19067f07
Sa318sha <kobrusevsasha318@gmail.com>
2022-04-15 15:08:17 -0600

    completed comments for Food Object

--- InventoryApplication/src/edu/ucalgary/ensf409/Food.java
+++ InventoryApplication/src/edu/ucalgary/ensf409/Food.java
@@ -1,5 +1,10 @@
 package edu.ucalgary.ensf409;
-
+/**
+ * 
+ * @author sasha
+ *	basically a final class that hold a nutrition object 
+ *	t
+ */
 public class Food {
 	private final Nutrition NUTRITIONVALUES;
 	private final String NAME;
@@ -10,13 +15,19 @@ public class Food {
 		this.NUTRITIONVALUES = nutrition;
 		this.NAME = name;
 		this.ID = ID;
+		//sets the final variables depending on the parameters passed in
 	}
+	//getter returns nutritional Field
 	public Nutrition getNutritionValues() {
 		return NUTRITIONVALUES;
+		
 	}
+	//getter returns name field
 	public String getName() {
+		
 		return NAME;
 	}
+	//getter returns ID field
 	public int getID() {
 		return ID;
 	}
